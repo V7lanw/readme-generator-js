404139c1c6c5229ac9e1d017551c2280b924d257
rbmilkveWin10 <v7lanw@hotmail.com>
2023-02-28 17:50:14 +0800

    finished speed run and refactoring renderLicenseSection function

--- index.js
+++ index.js
@@ -8,61 +8,67 @@ const questions = [
     {
         type: "input",
         name: "GitHub",
-        message: "What is your GitHub username? 🐱‍💻  "
+        message: "What is your GitHub username? 🐱‍💻  \n    "
     },
     {
         type: "input",
         name: "Email",
-        message: "What is your email address? ✉️  "
+        message: "What is your email address? ✉️  \n    "
     },
     {
         type: "input",
         name: "Title",
-        message: "What is your project's name? 📐🔬⚙️  "
+        message: "What is your project's name? (CAPITALIZE IT!) 📐🔬⚙️  \n    "
     },
     {
         type: "input",
         name: "Description",
-        message: "Please describe your project briefly? 📝  "
+        message: "Please describe your project briefly? 📝  \n    "
     },
     {
         type: "list",
         name: "License",
-        message: "Which is the license of your project? ®️  ",
-        choices: 
-        [
-            'None', 
-            'Apache License 2.0', 
-            'GNU General Public License v3.0', 
-            'MIT License', 
-            'BSD 2-Clause "Simplified" License',
-            'BSD 3-Clause "New" or "Revised" License',
-            'Boost Software License 1.0',
-            'Creative Commons Zero v1.0 Universal',
-            'Eclipse Public License 2.0',
-            'GNU Affero General Public License v3.0',
-            'GNU General Public License v2.0',
-            'GNU Lesser General Public License v2.1',
-            'Mozilla Public License 2.0',
-            'The Unlicense'
-        ]
+        message: "Which is the license of your project? ®️  \n    ",
+        choices:
+            [
+                'None',
+                'Apache License 2.0',
+                'GNU General Public License v3.0',
+                'MIT License',
+                'BSD 2-Clause "Simplified" License',
+                'BSD 3-Clause "New" or "Revised" License',
+                'Boost Software License 1.0',
+                'Creative Commons Zero v1.0 Universal',
+                'Eclipse Public License 2.0',
+                'GNU Affero General Public License v3.0',
+                'GNU General Public License v2.0',
+                'GNU Lesser General Public License v2.1',
+                'Mozilla Public License 2.0',
+                'The Unlicense'
+            ]
     },
     {
         type: "input",
-        name: "Test",
-        message: "What command should be run to test all mock cases? 🏗️  ",
+        name: "Installation",
+        message: "What command should be run to install all dependencies? 🏗️  \n    ",
+        default: "npm install ..."
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "What command should be run to test all mock cases? 🏗️  \n    ",
         default: "npm test ..."
     },
     {
         type: "input",
         name: "Usage",
-        message: "What is the objective of this project? 🎯 \nHow to play with your project code? 🕹️  ",
-        default: "npm install ..."
+        message: "What is the objective of this project? 🎯 \n  and how to play with your project code? 🕹️  \n    ",
+        default: "This project is about ...\nTo run it, install dependencies first with \`npm install\` ...\nthen type\n\n\`\`\` bash\nnpm ./index.js\n\`\`\`\n\nand follow the steps in CLI ..."
     },
     {
         type: "input",
         name: "Contributions",
-        message: "What is the future development plan of this project? 🏗️ \nHow to engage with your project code? 🛠️  ",
+        message: "What is the future development plan of this project? 🏗️ \n  and how to engage with your project code? 🛠️  \n    ",
         default: "npm install ... then npm test ..."
     }
 ];
@@ -76,7 +82,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions).then((response) => {
         console.log(`Generating README...`);
-        writeToFile("README.markdown", generateMarkdown({...response}));
+        writeToFile("README.markdown", generateMarkdown({ ...response }));
     });
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,85 @@
-const renderLicenseBadge = function (license) {
+const renderLicenseBadge = (license) => {
   if (license !== "None") {
-    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    let licenseURLCode = license.replaceAll(" ", "%20");
+    return `![GitHub license](https://img.shields.io/badge/license-${licenseURLCode}-blue.svg)`;
   }
   return "";
 };
 
+const renderLicenseLink = (license) => {
+  if (license !== "None") {
+    return `\n\n* [License](#license)`;
+  }
+  return "";
+};
+
+const renderLicenseSection = (license) => {
+  switch (license) {
+    case 'None':
+      return "None";
+      break;
+    case 'Apache License 2.0':
+      return 'Apache License 2.0\n\n+ Commercial use';
+  }
+};
+
 // function to generate markdown for README
 function generateMarkdown(data) {
+  console.log(data);
   return `# ${data.Title}
-  ${renderLicenseBadge()}
-  `;
+
+${renderLicenseBadge(data.License)}
+
+## Description
+
+${data.Description}
+
+### Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)${renderLicenseLink(data.License)}
+
+* [Contributions](#contributions)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\` bash
+${data.Installation}
+\`\`\`
+
+## Usage
+
+${data.Usage}
+
+## License
+
+${renderLicenseSection(data.License)}
+
+## Contributions
+
+${data.Contributions}
+
+## Tests
+
+To test all cases, run the following command:
+
+\`\`\` bash
+${data.Tests}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly (${data.Email}).
+
+You can find more of my work at [${data.GitHub}'s niches](https://github.com/${data.GitHub}?tab=repositories).
+`;
 }
 
 export default generateMarkdown;
